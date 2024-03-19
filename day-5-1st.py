bd7afd45fd0b376101c63c1f87fbbf5adac814ec
Titli03 <89897324+Titli03@users.noreply.github.com>
2024-03-19 12:08:03 -0600

    Add files via upload

---
+++ day-5-1st.py
@@ -0,0 +1,13 @@
+# Input a Python list of student heights
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+total1=0
+for total in student_heights:
+    total1+=total
+average_hight=round(total1/(n+1))
+
+print(f"total height = {total1}")
+print(f"number of students = {n+1}")
+print(f"average height = {average_hight}")
