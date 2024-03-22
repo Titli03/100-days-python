f4d507611b94ac025241b0371a6149c09f82885d
Titli03 <89897324+Titli03@users.noreply.github.com>
2024-03-22 11:33:39 -0600

    Add files via upload

---
+++ day-6-hurdle2.py
@@ -0,0 +1,18 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+number_of_hurdle=6
+while number_of_hurdle>0
+    jump()
+    number_of_hurdle-=1
+    
